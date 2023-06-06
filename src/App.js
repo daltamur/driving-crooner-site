ae39c9589561f34dff02954dd06cc7b89c2157a5
daltamur <daltamur@oswego.edu>
2023-06-06 16:43:22 -0400

    landing page container now centers perfectly on phones

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from './Components/LandingPage'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{height: '100vh'}}>
       <HeaderBar/>
       <LandingPage/>
     </div>
