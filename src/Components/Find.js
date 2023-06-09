ed66af9e623964d947137df6cd59441f1a4e4d6c
daltamur <daltamur@oswego.edu>
2023-06-09 15:46:54 -0400

    rebuilt

--- src/Components/Find.js
+++ src/Components/Find.js
@@ -3,12 +3,36 @@ import './../styles/styles.css'
 import marker from './../img/marker.png'
 
 function Find(){
+  let markers=[
+    {
+      id:1,
+      latitude: 35.143474,
+      longitude: -120.763247,
+      shelter:'marker 1'
+
+    },
+    {
+      id: 2,
+      latitude: 42.143474,
+      longitude: -110.763247,
+      shelter: 'marker 2'
+
+    },
+    {
+      id: 3,
+      latitude: 39.143474,
+      longitude: -137.763247,
+      shelter: 'marker 3'
+
+    }
+  ]
+
   const Marker = () => <div><img src={marker} alt={'marker'} className={"marker"}/></div>;
 
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627
+      lat: 34.143474,
+      lng: -118.763247
     },
     zoom: 80
   };
@@ -23,10 +47,14 @@ function Find(){
         bootstrapURLKeys={{key: process.env.REACT_APP_MAP_API_KEY}}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}>
-          <Marker
-            lat={10.99835602}
-            lng={77.01502627}
-          />
+          {markers.map(marker => {
+            return(
+              <Marker
+                key={marker.id}
+                position={{ lat: marker.latitude, lng: marker.longitude }}
+                />
+            )
+          })}
         </GoogleMapReact>
       </div>
     </div>
