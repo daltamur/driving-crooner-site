0973fe7e9cd7c277eba6d85e5c160353ab721f35
daltamur <daltamur@oswego.edu>
2023-06-09 22:25:28 -0400

    rebuilt with finished find the crooner page

--- src/Components/Become.js
+++ src/Components/Become.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap'
+import { Container, Form } from 'react-bootstrap'
 
 function Become(){
   return(
@@ -10,8 +10,22 @@ function Become(){
           </h1>
           <h3>Apply to become a driving crooner by filling out this form.</h3>
         </row>
-        <row>
 
+        <row>
+          <div className={'form'}>
+            <Form>
+              <div className={'form-body'}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Example textarea</Form.Label>
+                  <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+              </div>
+            </Form>
+          </div>
         </row>
       </Container>
     </div>
